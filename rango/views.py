741aa4180db9f935dd5eed0fa269b433fd10e69e
Jialin Zhang <2410728Z@student.gla.ac.uk>
2019-02-12 14:17:42 +0000

    Chapter 4 updata2

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! "
-                        "<br/> <a href='/rango/about/'>About</a>")
+    # Construct a dictionary for template engine
+    # the boldmessage is the same as {{boldmessage}} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page."
-                        "<a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
